0fee249c91843284b6166e0372186dc863911755
Barack Obama <95661329+redhots@users.noreply.github.com>
2024-08-05 10:56:48 -0400

    disabled nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   
   return (
     <div>
-    <Navbar/>
+   
     <Routes>
       <Route path="/" element= {<Home/>}></Route>
       <Route path="/memegallery" element={<MemeGallery/>}/>
